244a8da3a30789c74644dbc7c02c5418699c6d66
Rahul Shelke <srahul07@gmail.com>
2018-06-10 18:07:01 +0530

    Adding repository names for testing

--- build_creator.py
+++ build_creator.py
@@ -15,7 +15,7 @@ latest_subdir = max(all_subdirs_of("tests"), key=os.path.getmtime)
 job_name = latest_subdir.split(os.sep)[1]
 
 # Connect to Jenkins
-server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
+server = jenkins.Jenkins("http://localhost:8080", username="rahul", password="rahul")
 server.create_job(job_name, jenkins.EMPTY_CONFIG_XML)
 server.build_job(job_name)
 
