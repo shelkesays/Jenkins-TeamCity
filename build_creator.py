5eac50093d4da173eee65d47424e2cee7a72b1c9
Rahul Shelke <srahul07@gmail.com>
2018-06-10 17:32:23 +0530

    Adding line to show jobs created

--- build_creator.py
+++ build_creator.py
@@ -18,3 +18,6 @@ job_name = latest_subdir.split(os.sep)[1]
 server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
 server.create_job(job_name, jenkins.EMPTY_CONFIG_XML)
 server.build_job(job_name)
+
+jobs = server.get_jobs()
+print("Jobs: ", jobs)
