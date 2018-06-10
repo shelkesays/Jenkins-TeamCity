3d8e12a64ca66a3e5dcd713a2cb3cccfb764eebe
Rahul Shelke <srahul07@gmail.com>
2018-06-10 17:30:44 +0530

    Correcting build

--- build_creator.py
+++ build_creator.py
@@ -12,8 +12,9 @@ def all_subdirs_of(dir='.'):
 
 
 latest_subdir = max(all_subdirs_of("tests"), key=os.path.getmtime)
+job_name = latest_subdir.split(os.sep)[1]
 
 # Connect to Jenkins
 server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
-server.create_job(latest_subdir, jenkins.EMPTY_CONFIG_XML)
-server.build_job(latest_subdir)
+server.create_job(job_name, jenkins.EMPTY_CONFIG_XML)
+server.build_job(job_name)
