7e9c9cc9bee5753bcb733a8b9b3dd473147054b4
Rahul Shelke <srahul07@gmail.com>
2018-06-11 22:32:34 +0530

    Adding config in build creator

--- build_creator.py
+++ build_creator.py
@@ -1,14 +1,15 @@
 import os
 import jenkins
+import config
 
 
 def all_subdirs_of(dir='.'):
-  result = []
-  for item in os.listdir(dir):
-    path = os.path.join(dir, item)
-    if os.path.isdir(path):
-      result.append(path)
-  return result
+    result = []
+    for item in os.listdir(dir):
+        path = os.path.join(dir, item)
+        if os.path.isdir(path):
+            result.append(path)
+    return result
 
 
 # Check if last build was processed 2 days back
@@ -16,8 +17,8 @@ def all_subdirs_of(dir='.'):
 # Else, exit / return 0
 
 # latest_subdir = max(all_subdirs_of("tests"), key=os.path.getmtime)
-latest_subdir = all_subdirs_of("tests")
-job_file = open("processed.log", "r+")
+latest_subdir = all_subdirs_of(config.SUB_DIR_OF)
+job_file = open(config.PROCESSED_LOG, "r+")
 lines = job_file.readlines()
 # Strip all new lines
 lines = map(lambda s: s.strip(), lines)
@@ -29,12 +30,11 @@ else:
     recent_files = recent_files + latest_subdir
 # Close File
 job_file.close()
-#job_name = latest_subdir.split(os.sep)[1]
 
 # Connect to Jenkins
 # NOTE: Might need to change this for TeamCity
-server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
-job_file = open("processed.log", "a+")
+server = jenkins.Jenkins(config.SERVER_URL, username=config.SERVER_USER, password=config.SERVER_PASSWORD)
+job_file = open(config.PROCESSED_LOG, "a+")
 for item in recent_files:
     job = item.split(os.sep)[-1]
     # Create a job and make a build
