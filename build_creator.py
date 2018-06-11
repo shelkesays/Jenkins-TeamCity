7a4e27a359ac19991707c738d627d76b02798f1b
Rahul Shelke <srahul07@gmail.com>
2018-06-11 07:54:18 +0530

    Adding multiple folder processing

--- build_creator.py
+++ build_creator.py
@@ -12,12 +12,21 @@ def all_subdirs_of(dir='.'):
 
 
 latest_subdir = max(all_subdirs_of("tests"), key=os.path.getmtime)
-job_name = latest_subdir.split(os.sep)[1]
+job_file = open("processed.log", "+a")
+lines = job_file.read()
+recent_files = set(latest_subdir) & set(job_file)
+recent_files = list(recent_files)
+#job_name = latest_subdir.split(os.sep)[1]
 
 # Connect to Jenkins
 server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
-server.create_job(job_name, jenkins.EMPTY_CONFIG_XML)
-server.build_job(job_name)
 
+for item in recent_files:
+    job = item.split(os.sep)[-1]
+    server.create_job(job, jenkins.EMPTY_CONFIG_XML)
+    server.build_job(job)
+    job_file.write(item)
+
+job_file.close()
 jobs = server.get_jobs()
 print("Jobs: ", jobs)
