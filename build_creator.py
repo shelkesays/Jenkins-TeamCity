b0ee755dbbbe6fb542707ce3b15a516b2ded3c12
Rahul Shelke <srahul07@gmail.com>
2018-06-11 08:03:53 +0530

    Adding readlines

--- build_creator.py
+++ build_creator.py
@@ -14,19 +14,26 @@ def all_subdirs_of(dir='.'):
 latest_subdir = max(all_subdirs_of("tests"), key=os.path.getmtime)
 job_file = open("processed.log", "a+")
 lines = job_file.readlines()
+recent_files = []
 print(lines)
-recent_files = set(latest_subdir) - set(lines)
-print(recent_files)
-recent_files = list(recent_files)
-print(recent_files)
+if lines:
+    recent_files = set(latest_subdir) - set(lines)
+    print(recent_files)
+    recent_files = list(recent_files)
+    print(recent_files)
+else:
+    recent_files = recent_files + latest_subdir
 #job_name = latest_subdir.split(os.sep)[1]
 
 # Connect to Jenkins
+# NOTE: Might need to change this for TeamCity
 server = jenkins.Jenkins("http://localhost:8080", username="srahul07", password="rahul")
 
 for item in recent_files:
     job = item.split(os.sep)[-1]
     print(job)
+    # Create a job and make a build
+    # NOTE: Might need to change this for TeamCity
     server.create_job(job, jenkins.EMPTY_CONFIG_XML)
     server.build_job(job)
     job_file.write(item)
