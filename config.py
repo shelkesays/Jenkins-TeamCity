2cae11baeaec71f82eab631ae5bca2a96721b6d8
Rahul Shelke <srahul07@gmail.com>
2018-06-11 22:23:05 +0530

    Updating config

---
+++ config.py
@@ -0,0 +1,10 @@
+SERVER_PROTCOL = "http"
+SERVER_HOST = "localhost"
+SERVER_PORT = 8080
+SERVER_USER = "srahul07"
+SERVER_PASSWORD = "rahul"
+
+SERVER_URL = "{0}://{1}:{2}".format(SERVER_PROTCOL, SERVER_HOST, SERVER_PORT)
+
+LAST_PROCESSED_TIMESTAMP_LOG = "last_processed_timestamp.log"
+SUB_DIR_OF = "tests"
