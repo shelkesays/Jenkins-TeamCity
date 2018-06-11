7346557904843117436e6363334f20602cfc970e
Rahul Shelke <srahul07@gmail.com>
2018-06-11 22:35:56 +0530

    Adding git ignore

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ SERVER_PASSWORD = "rahul"
 
 SERVER_URL = "{0}://{1}:{2}".format(SERVER_PROTCOL, SERVER_HOST, SERVER_PORT)
 
+PROCESSED_LOG = "processed.log"
 LAST_PROCESSED_TIMESTAMP_LOG = "last_processed_timestamp.log"
 SUB_DIR_OF = "tests"
