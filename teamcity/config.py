56c1e37ffbf75029b0e2a3451d088043a5b9eba2
Rahul Shelke <srahul07@gmail.com>
2018-06-28 20:22:54 +0530

    Updating with comments fot things to changes

--- teamcity/config.py
+++ teamcity/config.py
@@ -1,25 +1,26 @@
 # Teamcity configurations
-SERVER_PROTCOL = "http"
-SERVER_HOST = "localhost"
-SERVER_PORT = 8111
-SERVER_USER = "srahul07"
-SERVER_PASSWORD = "rahul"
+SERVER_PROTCOL = "http"  # XXXXXX
+SERVER_HOST = "localhost"  # XXXXXX
+SERVER_PORT = 8111  # XXXXXX
+SERVER_USER = "srahul07"  # XXXXXX
+SERVER_PASSWORD = "rahul"  # XXXXXX
 
 SERVER_URL = "{0}://{1}:{2}".format(SERVER_PROTCOL, SERVER_HOST, SERVER_PORT)
 
 PROCESSED_LOG = "processed.log"
-LAST_PROCESSED_TIMESTAMP_LOG = "last_processed_timestamp.log"
-SUB_DIR_OF = "tests"
+LAST_PROCESSED_TIMESTAMP_LOG = "last_processed_timestamp.log" # XXXXXX
+SUB_DIR_OF = "testx"  # XXXXXX
 
 COPY_BUILDTYPE_XML = "copybuildtype.xml"
 TRIGGER_BUILD_XML = "triggerbuild.xml"
-PROJECT_ID = "XmlImport"
+PROJECT_ID = "XmlImport"  # XXXXXX
 SOURCE_ATTRIBUTE = "sourceBuildTypeLocator"
 TARGET_ATTRIBUTE = "name"
-SOURCE_BUILD_TYPE_ID = "XmlImport_Template"  # "XmlImport_Builder"
+SOURCE_BUILD_TYPE_ID = "XmlImport_Builder"  # XXXXXX
+
 TRIGGER_BUILDTYPE_ATTRIBUTE = "buildType"
 SOURCE_TRIGGER_BUILD_ATTRIBUTE = "id"
 TARGET_TRIGGER_BUILD_ATTRIBUTE = "id"
 
 # Project configuration in xml
-XML_PROJECT_CONFIG_FILE = "project_config.xml"
+XML_PROJECT_CONFIG_FILE = "project_config.xml"  # XXXXXX give your xml file configuration
